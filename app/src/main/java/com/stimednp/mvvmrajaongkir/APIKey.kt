7970c56688f27c21e0cb6456d13fbbed9d8669f3
rivaldy <frizgt@gmail.com>
2022-09-15 10:08:19 +0800

    :closed_lock_with_key: only add file api key

---
+++ app/src/main/java/com/stimednp/mvvmrajaongkir/APIKey.kt
@@ -0,0 +1,9 @@
+package com.stimednp.mvvmrajaongkir
+
+/** Created by github.com/im-o on 9/15/2022. */
+
+class APIKey {
+    companion object {
+        const val API_KEY = "REPLACE WITH YOUR API KEY..."
+    }
+}
